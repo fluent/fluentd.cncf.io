1d2622f530b33ece6d15ff9eaf2a7d04f05318e2
Kentaro Hayashi <hayashi@clear-code.com>
2025-08-26 14:44:30 +0900

    Update fetching apt-source
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- sh/install-ubuntu-jammy-fluent-package6.sh
+++ sh/install-ubuntu-jammy-fluent-package6.sh
@@ -10,7 +10,7 @@ sudo -k
 # run inside sudo
 sudo sh <<SCRIPT
   # use apt-source package which contains keyring
-  curl -o fluent-apt-source.deb https://fluentd.cdn.cncf.io/6/ubuntu/jammy/pool/contrib/f/fluent-apt-source/fluent-apt-source_2025.7.29-1_all.deb
+  curl -o fluent-apt-source.deb https://fluentd.cdn.cncf.io/6/ubuntu/jammy/pool/contrib/f/fluent-apt-source/fluent-apt-source_2025.8.29-1_all.deb
   apt install -y ./fluent-apt-source.deb
   # update your sources
   apt update
