9557b6c4c19e10196c62e28c04aa8c5e4942ba09
Kentaro Hayashi <hayashi@clear-code.com>
2025-08-28 11:45:33 +0900

    Apply assumeyes to install rpm
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- sh/install-redhat-fluent-package6-lts.sh
+++ sh/install-redhat-fluent-package6-lts.sh
@@ -14,7 +14,7 @@ sudo sh <<'SCRIPT'
   version=$(cat /etc/system-release-cpe | awk '{print substr($1, index($1, "o"))}' | cut -d: -f4)
   arch=$(rpm --eval %{_arch})
   curl --silent -o fluent-release.rpm https://fluentd.cdn.cncf.io/lts/6/redhat/${version}/${arch}/fluent-lts-release-2025.8.29-1.el${version}.noarch.rpm
-  yum install ./fluent-release.rpm
+  yum install -y ./fluent-release.rpm
   rm -f ./fluent-release.rpm
 
   # update your sources
