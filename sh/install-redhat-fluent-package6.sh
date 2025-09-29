450e42a1ca766587a54032fcc2e366b48cb4cd3b
Kentaro Hayashi <hayashi@clear-code.com>
2025-09-29 14:23:50 +0900

    Bump version to 2025.9.29 (#1)
    * fluent-apt-source
      * Add Architectures: field
    * fluent-release,fluent-lts-release
      * Drop needless dependencies
    
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- sh/install-redhat-fluent-package6.sh
+++ sh/install-redhat-fluent-package6.sh
@@ -13,7 +13,7 @@ sudo sh <<'SCRIPT'
   distribution=$(cat /etc/system-release-cpe | awk '{print substr($1, index($1, "o"))}' | cut -d: -f2)
   version=$(cat /etc/system-release-cpe | awk '{print substr($1, index($1, "o"))}' | cut -d: -f4)
   arch=$(rpm --eval %{_arch})
-  curl --silent -o fluent-release.rpm https://fluentd.cdn.cncf.io/6/redhat/${version}/${arch}/fluent-release-2025.8.29-1.el${version}.noarch.rpm
+  curl --silent -o fluent-release.rpm https://fluentd.cdn.cncf.io/6/redhat/${version}/${arch}/fluent-release-2025.9.29-1.el${version}.noarch.rpm
   if [ -e /etc/yum.repos.d/fluent-package.repo ]; then
     if ! rpm -qf /etc/yum.repos.d/fluent-package.repo; then
       echo "Backup unmanaged .repo to fluent-package.repo.rpmsave"
