1f769d498f8aa2181e597250ed47a58062fd1310
Kentaro Hayashi <hayashi@clear-code.com>
2025-09-29 10:36:00 +0900

    Add updated install scripts
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- sh/install-amazon2023-fluent-package6-lts.sh
+++ sh/install-amazon2023-fluent-package6-lts.sh
@@ -13,7 +13,7 @@ sudo sh <<'SCRIPT'
   # add fluent-release to access repository
   version=$(cat /etc/system-release-cpe | awk '{print substr($1, index($1, "o"))}' | cut -d: -f4)
   arch=$(rpm --eval %{_arch})
-  curl -o fluent-release.rpm https://fluentd.cdn.cncf.io/lts/6/amazon/${version}/${arch}/fluent-lts-release-2025.8.29-1.amzn${version}.noarch.rpm
+  curl -o fluent-release.rpm https://fluentd.cdn.cncf.io/lts/6/amazon/${version}/${arch}/fluent-lts-release-2025.9.29-1.amzn${version}.noarch.rpm
   if [ -e /etc/yum.repos.d/fluent-package-lts.repo ]; then
     if ! rpm -qf /etc/yum.repos.d/fluent-package-lts.repo; then
       echo "Backup unmanaged .repo to fluent-package-lts.repo.rpmsave"
