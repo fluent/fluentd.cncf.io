a5b5eb4ae2ee24dd36d205aa3d21f2de6ff3971e
Kentaro Hayashi <hayashi@clear-code.com>
2025-10-09 10:48:17 +0900

    Support lts/v5 again (#8)
    It is useful if it supports to test with lts/5 too.
    
    ---------
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- test-verify-repo.sh
+++ test-verify-repo.sh
@@ -4,7 +4,27 @@
 # Usage: test-verify-repo.sh 6.0.0
 #
 # It try to verify whether fluent-package is installable or not
-# from test/experimental/6, test/experimental/lts/6
+# from test/experimental/6, test/experimental/lts/6 and so on.
+#
+# You can customize test targets via environment variables.
+#
+# REPO_TARGETS: It specify the target repositories (e.g. lts/5, lts/6, exp/6, exp/lts/6)
+# DEB_TARGETS: It specify the target debian releases (e.g. debian:trixie, ubuntu:noble)
+#              If you want to skip it, set DEB_TARGETS=dummy
+# RPM_TARGETS: It specify the target RHEL compatible releases (e.g. almalinux:8)
+#              If you want to skip it, set RPM_TARGETS=dummy
+#
+# Case 1: test recent v5/LTS
+#
+#   REPO_TARGETS=lts/5 ./test-verify-repo.sh 5.0.8
+#
+# Case 2: test recent v6/LTS (only deb) on test/experimental/lts/6
+#
+#   REPO_TARGETS=exp/lts/6 RPM_TARGETS=dummy ./test-verify-repo.sh 6.0.0
+#
+# Case 3: test recent v6/LTS (only rpm) on test/experimental/lts/6
+#
+#   REPO_TARGETS=exp/lts/6 DEB_TARGETS=dummy ./test-verify-repo.sh 6.0.0
 #
 
 function test_deb() {
@@ -50,16 +70,29 @@ fi
 
 VERSION=$1
 
-if [ -z "$DEB_TARGETS" ]; then
-    DEB_TARGETS="debian:bookworm debian:trixie ubuntu:jammy ubuntu:noble"
-fi
-if [ -z "$RPM_TARGETS" ]; then
-    RPM_TARGETS="rockylinux:8 almalinux:9 almalinux:10 amazonlinux:2023"
-fi
 if [ -z "$REPO_TARGETS" ]; then
     REPO_TARGETS="exp/6 exp/lts/6"
 fi
-
+if [ -z "$DEB_TARGETS" ]; then
+    case $REPO_TARGETS in
+	*6*)
+	    DEB_TARGETS="debian:bookworm debian:trixie ubuntu:jammy ubuntu:noble"
+	    ;;
+	*5*)
+	    DEB_TARGETS="debian:bullseye debian:bookworm ubuntu:focal ubuntu:jammy ubuntu:noble"
+	    ;;
+    esac
+fi
+if [ -z "$RPM_TARGETS" ]; then
+    case $REPO_TARGETS in
+	*6*)
+	    RPM_TARGETS="rockylinux:8 almalinux:9 almalinux:10 amazonlinux:2023"
+	    ;;
+	*5*)
+	    RPM_TARGETS="rockylinux:8 almalinux:9 amazonlinux:2 amazonlinux:2023"
+	    ;;
+    esac
+fi
 echo "DEB_TARGETS: $DEB_TARGETS"
 echo "RPM_TARGETS: $RPM_TARGETS"
 echo "REPO_TARGETS: $REPO_TARGETS"
