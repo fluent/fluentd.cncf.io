f66a47d84d8694fdf730d96fedd127cff2f6bca6
Kentaro Hayashi <hayashi@clear-code.com>
2025-10-09 10:29:49 +0900

    Add more usages
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- test-verify-repo.sh
+++ test-verify-repo.sh
@@ -4,7 +4,27 @@
 # Usage: test-verify-repo.sh 6.0.0
 #
 # It try to verify whether fluent-package is installable or not
-# from test/experimental/6, test/experimental/lts/6
+# from test/experimental/6, test/experimental/lts/6 and so on.
+#
+# You can customize test targets via environment variables.
+#
+# REPO_TARGETS: It specify the target repositories (e.g. lts/5, lts/6, exp/6, exp/lts/6)
+# DEB_TARGETS: It specify the target debian releases (e.g. debian:trixie, ubuntu:noble)
+#              If you want to skip it, set DEB_TARGETS=dummy
+# RPM_TARGETS: It specify the target RHEL compatible releases (e.g. almalinux:8)
+#              If you want to skip it, set RPM_TARGETS=dummy
+#
+# Case 1: test recent v5/LTS
+#
+#   REPO_TARGETS=lts/5 ./test-verify-repo.sh 5.0.8
+#
+# Case 2: test recent v6/LTS (only deb) on test/experimental/lts/6
+#
+#   REPO_TARGETS=exp/lts/6 RPM_TARGETS=dummy ./test-verify-repo.sh 6.0.0
+#
+# Case 3: test recent v6/LTS (only rpm) on test/experimental/lts/6
+#
+#   REPO_TARGETS=exp/lts/6 DEB_TARGETS=dummy ./test-verify-repo.sh 6.0.0
 #
 
 function test_deb() {
